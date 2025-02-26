b2fac2fdbcf20a33b3e6f1f5714abc730b1df60b
SzymonKonieczny <kowladMC@gmail.com>
2025-02-26 20:55:56 +0100

    fixed sdpawning

--- RubyArenaProject/Assets/Scripts/Player/PlayerScript.cs
+++ RubyArenaProject/Assets/Scripts/Player/PlayerScript.cs
@@ -11,14 +11,29 @@ public class PlayerScript : NetworkBehaviour
     public Transform ModelAnchor;
     public NetworkVariable <int> CharacterID;
     [SerializeField] PlayerAnimationScript PlayerAnimationScript;
+    public Transform ActiveModel;
+
+    protected override void OnNetworkPostSpawn()
+    {
+        if (IsServer)
+        {
+            CharacterID.Value = NetworkManager.ConnectedClients.Count;
+        }
+
+    }
+
     public override void OnNetworkSpawn()
     {
 
-        if (IsServer)
-        {
-            CharacterID.Value = 0; //NetworkManager.ConnectedClients.Count-1; 
-            InstantiateModelClientRPC(CharacterID.Value);
-        }
+    //    CharacterID.OnValueChanged += (int pre, int post) =>
+    //    {
+    //        InstantiateModel(post);
+    //    };
+
+        InstantiateModel(0);
+
+
+
         if (!IsLocalPlayer)
         {
             
@@ -30,11 +45,15 @@ public class PlayerScript : NetworkBehaviour
         }
 
     }
-   [ClientRpc]
-    void InstantiateModelClientRPC(int id)
+    void InstantiateModel(int id)
     {
-        GameObject ModelGO = Instantiate(CharacterList.Instance.Chracters[CharacterID.Value].Model,ModelAnchor);
+        if (ActiveModel != null)
+        {
+            Destroy(ActiveModel);
+        }
+        GameObject ModelGO = Instantiate(CharacterList.Instance.Chracters[CharacterID.Value % 2].Model,ModelAnchor);
         ModelGO.transform.SetLocalPositionAndRotation(new Vector3(0, -1, 0), Quaternion.Euler(0, 0, 0));
+        ActiveModel = ModelGO.transform;
         PlayerAnimationScript = GetComponent<PlayerAnimationScript>();
 
         Animator animator = ModelGO.transform.GetComponentInChildren<Animator>();
