0669fd459a876917fe6b161f7c1b1eb0c0fd241a
SzymonKonieczny <kowladMC@gmail.com>
2025-06-15 20:49:57 +0200

    dasdas

---
+++ RubyArenaProject/Assets/PlayerSkillHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkillHolder : MonoBehaviour
+{
+    public PlayerCombatManager playerCombatManager;
+    public InputCollectorScript inputCollectorScript;
+}
