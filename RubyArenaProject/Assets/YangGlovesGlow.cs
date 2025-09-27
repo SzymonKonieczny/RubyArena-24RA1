a98af0af5c532ff8c63578c4e5e9f61e8d218361
SzymonKonieczny <kowladMC@gmail.com>
2025-09-27 18:28:10 +0200

    Minor Yang nerfs

---
+++ RubyArenaProject/Assets/YangGlovesGlow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YangGlovesGlow : MonoBehaviour, ISkillEffect
+{
+    [SerializeField] Material mat;
+    public void PlayEffect(int id)
+    {
+       //s mat.set
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
